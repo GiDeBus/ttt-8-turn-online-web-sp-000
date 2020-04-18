cd5a9567d3af6246124b2cdc319004793dd7b19a
immutable-integer-8885 <gidelbusto@gmail.com>
2020-04-18 21:29:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -51,8 +51,10 @@ end
 #turn
 
 def turn(board)
+  
   puts "Please enter 1-9:"
   user_input = gets.strip
+  
   index = input_to_index(user_input)
   if valid_move?(board,index)
     move(board,input_to_index,players_character ='X') 
